04533085e7d1cb4a6a33672ce11344c5d0f0fdf7
Documenter.jl <documenter@juliadocs.github.io>
2025-03-24 20:08:48 +0000

    build based on d8f676c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = C13NV","category":"page"},{"location":"#C13NV","page":"Home","title":"C13NV","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for C13NV.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [C13NV]","category":"page"}]
+}
