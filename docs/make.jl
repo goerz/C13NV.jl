f453ef04f4e4b6f981dc798d1d63755912818341
Michael Goerz <mail@michaelgoerz.net>
2025-03-24 14:15:37 -0400

    Customize package setup

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,31 @@
 using C13NV
 using Documenter
+import Pkg
 
-DocMeta.setdocmeta!(C13NV, :DocTestSetup, :(using C13NV); recursive=true)
+
+PROJECT_TOML = Pkg.TOML.parsefile(joinpath(@__DIR__, "..", "Project.toml"))
+VERSION = PROJECT_TOML["version"]
+NAME = PROJECT_TOML["name"]
+AUTHORS = join(PROJECT_TOML["authors"], ", ") * " and contributors"
+GITHUB = "https://github.com/goerz/C13NV.jl"
+
+println("Starting makedocs")
+
+PAGES = ["Home" => "index.md",]
 
 makedocs(;
-    modules=[C13NV],
-    authors="Michael Goerz <mail@michaelgoerz.net> and contributors",
-    sitename="C13NV.jl",
-    format=Documenter.HTML(;
-        canonical="https://goerz.github.io/C13NV.jl",
-        edit_link="master",
-        assets=String[],
+    authors = "Michael Goerz <mail@michaelgoerz.net> and contributors",
+    sitename = "C13NV.jl",
+    format = Documenter.HTML(;
+        prettyurls = true,
+        canonical = "https://goerz.github.io/C13NV.jl",
+        edit_link = "master",
+        footer = "[$NAME.jl]($GITHUB) v$VERSION docs powered by [Documenter.jl](https://github.com/JuliaDocs/Documenter.jl).",
+        assets = String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = PAGES,
 )
 
-deploydocs(;
-    repo="github.com/goerz/C13NV.jl",
-    devbranch="master",
-)
+println("Finished makedocs")
+
+deploydocs(; repo = "github.com/goerz/C13NV.jl", devbranch = "master", push_preview = true)
